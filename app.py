297138617ad36d54558758a5935eaffaef5c96e3
Matt Lane <lane.matthew@gmail.com>
2017-04-03 22:05:26 -0700

    draft auth flow on server

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, request, jsonify
 from flask_cors import CORS
-from spotipy import util, Spotify, oauth2
+from spotipy import oauth2, client
 import os
 
 app = Flask(__name__)
@@ -20,6 +20,10 @@ def login():
 @app.route('/authenticate', methods=["POST"])
 def authenticate():
   response = spotify_auth.get_access_token(request.json['code'])
+  username = client.Spotify(
+    auth=response['access_token']
+  ).me()['id']
+  response['username'] = username
   return jsonify(response)
 
 if __name__ == '__main__':
